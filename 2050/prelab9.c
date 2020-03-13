202b1193073b2c33955617a3a6a44529b28c353f
Adam <adam.j.menker@gmail.com>
2020-03-13 00:40:04 -0500

    partial solution updated

--- 2050/prelab9.c
+++ 2050/prelab9.c
@@ -23,14 +23,21 @@ int main(void){
     linkedList = createList();
 
     printList(linkedList);
+
+    int checkHead = insertAtHead(4, linkedList);
+    insertAtHead(3, linkedList);
+    insertAtHead(2, linkedList);
+    //printf("%d\n", checkHead);
+    printList(linkedList);
     int length = getListLength(linkedList);
     printf("%d\n", length);
 
-    int checkHead = insertAtHead(3, linkedList);
-    printf("%d\n", checkHead);
+    int checkTail = insertAtTail(5, linkedList);
+    //printf("%d\n", checkTail);
+    printList(linkedList);
 
-    int checkTail = insertAtTail(3, linkedList);
-    printf("%d\n", checkTail);
+    length = getListLength(linkedList);
+    printf("%d\n", length);
 
 }
 
@@ -46,9 +53,10 @@ List * createList(){ // implemented with a dummy node
 void printList(List * head){
     List * temp = head->next; // jumps past dummy node
     while(temp != NULL){
-        printf("%d\n", temp->data);
+        printf("%d - ", temp->data);
         temp = temp->next;
     }
+    printf("\n");
 }
 
 
@@ -60,10 +68,14 @@ int getListLength(List * head){
 
 int insertAtHead(int data, List * head){
     List * temp = (List *) malloc(sizeof(List));
+    if(temp == NULL){
+        printf("malloc failed");
+    }
     temp->data = data;
-    temp->next = head->next->next;
-    head->next = temp;
-    if(head->next == temp && temp->data == data && head->next->next == temp->next){ //checks if data was inserted and if list pointer were reassigned properly
+    temp->next = (*head).next;
+    (*head).next = temp;
+    if((*head).next == temp && temp->data == data && (*head).next->next == temp->next){ //checks if data was inserted and if list pointer were reassigned properly
+        head->data++; // incrementing size
         return 0;
     }
     return 1;
@@ -71,19 +83,25 @@ int insertAtHead(int data, List * head){
 
 
 int insertAtTail(int data, List * head){
-    List * newTail = (List *) malloc(sizeof(List));
-    newTail = head;
+    List * newTail = (List *) malloc(sizeof(List)); 
     List * temp;
-    while(newTail != NULL){
-        newTail = newTail->next;
-        if(newTail->next != NULL){
-            temp = newTail;
-        }
+    temp = head;
+    while(temp->next != NULL){
+        temp = temp->next; // breaks once temp is a pointer to last node
     }
-    temp->next = newTail;
     newTail->data = data;
-    if(newTail->next == NULL && newTail->data == data && temp->next->next == NULL){
+    newTail->next = NULL;
+    temp->next = newTail;
+    
+    
+    if(newTail->next == NULL && newTail->data == data && temp->next->next == NULL){//checks if data was inserted and if list pointer were reassigned properl
+        head->data++;
         return 0;
     }
     return 1;
+}
+
+
+List * copyList(List * copied){
+    return 0;
 }
